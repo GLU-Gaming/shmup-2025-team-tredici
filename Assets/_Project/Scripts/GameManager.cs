05e0bdd0a9a9219fd1f8b8acf60258186cc83585
Dooruss <doorusdebeijer@gmail.com>
2025-03-21 10:47:43 +0100

    Lifes

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI TextofLifes; //TEMP - Will become objects
     public int CurrentScore;
     public int CurrentLifes = 3;
+    [SerializeField] private GameObject[] Hearts;
 
     void Start()
     {
@@ -21,6 +23,17 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(2);
 
+        }
+
+        if (CurrentLifes == 2)
+        {
+            Hearts[2].SetActive(false);
+            Hearts[5].SetActive(true);
+        }
+        else if (CurrentLifes == 1)
+        {
+            Hearts[1].SetActive(false);
+            Hearts[4].SetActive(true);
         }
     }
 }
