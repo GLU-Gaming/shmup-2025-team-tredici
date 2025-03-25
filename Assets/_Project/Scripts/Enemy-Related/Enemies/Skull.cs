5f7d932510f614471c9476f6da93a85374811ea5
Dooruss <doorusdebeijer@gmail.com>
2025-03-25 10:26:39 +0100

    Skull = creeper

--- Assets/_Project/Scripts/Enemy-Related/Enemies/Skull.cs
+++ Assets/_Project/Scripts/Enemy-Related/Enemies/Skull.cs
@@ -4,12 +4,19 @@ public class Skull : EnemyBase
 {
     public override void EnemyMovement()
     {
-        // Goes towards you 
-        base.EnemyMovement();
         EnemyMaxHP = 2;
+        // The Skull goes towards the player and then explodes
+        // CREDITS: https://www.youtube.com/watch?v=ZExSz7x69j8
+        if (PlayerCharachter)
+        {
+            Vector3 direction = (PlayerCharachter.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            moveDirection = direction;
+        }
+        rb.linearVelocity = new Vector3(moveDirection.x, moveDirection.y) * MoveSpeed;
     }
     public override void EnemyAttack()
     {
-        base.EnemyAttack();
+       //n/a
     }
 }
