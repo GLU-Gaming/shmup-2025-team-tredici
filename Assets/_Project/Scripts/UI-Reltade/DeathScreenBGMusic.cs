b3408d6e928d384e62e8da463c5a7d845c6c86a3
Dooruss <doorusdebeijer@gmail.com>
2025-04-07 11:48:12 +0200

    deathscreen music fix

---
+++ Assets/_Project/Scripts/UI-Reltade/DeathScreenBGMusic.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DeathScreenBGMusic : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+    public GameObject bgmusic;
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+        
+    }
+
+    private void Update()
+    {
+        if (m_AudioSource.isPlaying)
+        {
+
+        } else
+        {
+            gameObject.SetActive(false);
+            bgmusic.SetActive(true);
+        }
+    } 
+}
